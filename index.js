c1f68563f5e3833cbf0653af8f96480f033088ba
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-03-11 21:56:35 +0530

    connect backend to MongoDB and move db configuration to model

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+require('dotenv').config()
 const express = require('express')
+const Note = require('./models/note')
 const cors = require('cors')
 const app = express()
+
 
 app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
+
+
 
 let notes = [
   {
@@ -29,17 +34,24 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  //response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find(note => note.id === id)
-   if (note) {
+  // const id = request.params.id
+  // const note = notes.find(note => note.id === id)
+  // if (note) {
+  //   response.json(note)
+  // } else {
+  //   response.status(404).end()
+  // }
+
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -60,23 +72,32 @@ app.post('/api/notes', (request, response) => {
   const body = request.body
 
   if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
+    return response.status(400).json({
+      error: 'content missing'
     })
   }
 
-  const note = {
+  // const note = {
+  //   content: body.content,
+  //   important: body.important || false,
+  //   id: generateId(),
+  // }
+
+  // notes = notes.concat(note)
+
+  // response.json(note)
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 
-  response.json(note)
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
