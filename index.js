015a360fea45feeee1e92d96befdf7d0254d9302
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-03-14 21:15:07 +0530

    feat: add validaton for content in note schema definition and expand error handler

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ const generateId = () => {
   return String(maxId + 1)
 }
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
   if (!body.content) {
@@ -103,7 +103,7 @@ app.post('/api/notes', (request, response) => {
 
   note.save().then(savedNote => {
     response.json(savedNote)
-  })
+  }).catch(error => next(error))
 
 })
 
@@ -132,7 +132,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
   next(error)
 }
