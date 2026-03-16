117d8e4b7cea3f37fa365ee44d8d6ff61629c7ea
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-03-16 16:43:04 +0530

    refactor project structure by separating controllers, models, and utils

--- models/note.js
+++ models/note.js
@@ -1,24 +1,12 @@
+//Contains MongoDB schemas/Database structure
+
 const mongoose = require('mongoose')
-
-mongoose.set('strictQuery', false)
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-mongoose.connect(url, { family: 4 })
-
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
 
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    minLength: 5,
-    required: true
+    required: true,
+    minlength: 5
   },
   important: Boolean,
 })
@@ -30,6 +18,5 @@ noteSchema.set('toJSON', {
     delete returnedObject.__v
   }
 })
-
 
 module.exports = mongoose.model('Note', noteSchema)
