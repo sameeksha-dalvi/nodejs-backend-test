0296ed1e023b067b0a99f698f8baf483370fd84f
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-03-20 18:56:14 +0530

    refactor: the add note route with async and await

--- controllers/notes.js
+++ controllers/notes.js
@@ -23,7 +23,7 @@ notesRouter.get('/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-notesRouter.post('/', (request, response, next) => {
+notesRouter.post('/', async (request, response) => {
   const body = request.body
 
   const note = new Note({
@@ -31,11 +31,13 @@ notesRouter.post('/', (request, response, next) => {
     important: body.important || false,
   })
 
-  note.save()
-    .then(savedNote => {
-      response.status(201).json(savedNote)
-    })
-    .catch(error => next(error))
+  // note.save()
+  //   .then(savedNote => {
+  //     response.status(201).json(savedNote)
+  //   })
+  //   .catch(error => next(error))
+  const savedNote = await note.save()
+  response.status(201).json(savedNote)
 })
 
 notesRouter.delete('/:id', (request, response, next) => {
