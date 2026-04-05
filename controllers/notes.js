f3f0005c86a04b00a9dec2d4637ac230b7f05d0b
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-04-05 18:36:54 +0530

    feat: update create note route to store the userId as well

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,6 +1,7 @@
 //controllers: Handles routes / API endpoints/ API logic
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 notesRouter.get('/', async (request, response) => {
   // Note.find({}).then(notes => {
@@ -33,9 +34,15 @@ notesRouter.get('/:id', async (request, response) => {
 notesRouter.post('/', async (request, response) => {
   const body = request.body
 
+  const user = await User.findById(body.userId)
+  if (!user) {
+    return response.status(400).json({ error: 'userId missing or not valid' })
+  }
+
   const note = new Note({
     content: body.content,
     important: body.important || false,
+    user: user._id
   })
 
   // note.save()
@@ -44,6 +51,8 @@ notesRouter.post('/', async (request, response) => {
   //   })
   //   .catch(error => next(error))
   const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
   response.status(201).json(savedNote)
 })
 
