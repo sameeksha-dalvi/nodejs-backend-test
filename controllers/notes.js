5b862dadab3e2e72b19860aa3ceca04abaf10b7e
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-08-23 23:19:20 +0530

    chore: add backend test mode script to learn playwright

--- controllers/notes.js
+++ controllers/notes.js
@@ -2,6 +2,17 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+
+const getTokenFrom = request =>{
+  const authorization = request.get('authorization')
+
+  if(authorization && authorization.startsWith('Bearer ')){
+    return authorization.replace('Bearer ','')
+  }
+  return null
+}
 
 notesRouter.get('/', async (request, response) => {
   // Note.find({}).then(notes => {
@@ -33,8 +44,11 @@ notesRouter.get('/:id', async (request, response) => {
 
 notesRouter.post('/', async (request, response) => {
   const body = request.body
-
-  const user = await User.findById(body.userId)
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
   if (!user) {
     return response.status(400).json({ error: 'userId missing or not valid' })
   }
