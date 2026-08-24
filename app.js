c307ad422ef1d53c90a64982afa97c01dd1001cc
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-08-24 17:46:03 +0530

    fix: add missing slash to testing API route

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use('/api/login', loginRouter)
 
 if (process.env.NODE_ENV === 'test'){
   const testingRouter = require('./controllers/testing')
-  app.use('api/testing', testingRouter)
+  app.use('/api/testing', testingRouter)
 }
 
 app.use(middleware.unknownEndpoint)
