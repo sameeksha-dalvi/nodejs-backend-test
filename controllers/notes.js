c009723db5e52b882259a04ba2b1c20d4d1bfea4
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-08-24 17:20:24 +0530

    test: add endpoint to reset database for E2E tests

--- controllers/notes.js
+++ controllers/notes.js
@@ -5,7 +5,7 @@ const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
 
-const getTokenFrom = request =>{
+const getTokenFrom = request => {
   const authorization = request.get('authorization')
 
   if(authorization && authorization.startsWith('Bearer ')){
